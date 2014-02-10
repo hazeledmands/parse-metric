1ccc3330fda41e59a1252def7be5dc25c7c131cc
Max Edmands <me@maxedmands.com>
2014-02-09 22:43:28 -0800

    Support for negative numbers

--- test/bytes.js
+++ test/bytes.js
@@ -1,24 +1,44 @@
 parse = require('../');
 
 describe('bytes', function() {
-  it('should convert a byte string into a Number', function() {
+  it('B', function() {
     assert.equal(parse('12543B'), 12543);
   });
 
-  it('should convert a kB string into a Number', function() {
+  it('bytes', function() {
+    assert.equal(parse('200 bytes'), 200);
+  });
+
+  it('kB', function() {
     assert.equal(parse('12kB'), 12 * 1000);
   });
 
-  it('should convert an MB string into a Number', function() {
+  it('kilobytes', function() {
+    assert.equal(parse('43 kilobytes'), 43 * 1000);
+  });
+
+  it('MB', function() {
     assert.equal(parse('23MB'), 23 * Math.pow(1000, 2));
   });
 
-  it('should convert a GB string into a Number', function() {
+  it('megabytes', function() {
+    assert.equal(parse('99 megabytes'), 99 * Math.pow(1000, 2));
+  });
+
+  it('GB', function() {
     assert.equal(parse('87GB'), 87 * Math.pow(1000, 3));
   });
 
-  it('should convert a TB string into a Number', function() {
+  it('gigabytes', function() {
+    assert.equal(parse('3 gigabytes'), 3 * Math.pow(1000, 3));
+  });
+
+  it('TB', function() {
     assert.equal(parse('87TB'), 87 * Math.pow(1000, 4));
+  });
+
+  it('terabytes', function() {
+    assert.equal(parse('42 terabytes'), 42 * Math.pow(1000, 4));
   });
 });
 
